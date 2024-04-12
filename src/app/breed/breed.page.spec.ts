92ea876e3a5aed3548c7b25f183d6b22292d471e
jdegand <70610011+jdegand@users.noreply.github.com>
2024-04-12 14:01:18 -0400

    feat: add more breedpage tests

--- src/app/breed/breed.page.spec.ts
+++ src/app/breed/breed.page.spec.ts
@@ -2,12 +2,15 @@ import { HttpClient } from "@angular/common/http";
 import { HttpTestingController, HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { BreedPage } from "./breed.page";
+import { ApiHttpClientService } from "../services/api-http-client.service";
+import { of } from "rxjs";
 
 describe('BreedPage', () => {
   let component: BreedPage;
   let fixture: ComponentFixture<BreedPage>;
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  let apiHttpClientService: ApiHttpClientService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,6 +21,7 @@ describe('BreedPage', () => {
     component = fixture.componentInstance;
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
+    apiHttpClientService = TestBed.inject(ApiHttpClientService);
   });
 
   afterEach(() => {
@@ -83,4 +87,419 @@ describe('BreedPage', () => {
 
     expect(component.breed).toEqual([DATA]);
   });
+
+  it('should fetch breed data and images correctly', () => {
+    const breedData = [
+      {
+        "weight": {
+          "imperial": "7 - 11",
+          "metric": "3 - 5"
+        },
+        "id": "kora",
+        "name": "Korat",
+        "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+        "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+        "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+        "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+        "origin": "Thailand",
+        "country_codes": "TH",
+        "country_code": "TH",
+        "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+        "life_span": "10 - 15",
+        "indoor": 0,
+        "adaptability": 5,
+        "affection_level": 5,
+        "child_friendly": 4,
+        "dog_friendly": 5,
+        "energy_level": 3,
+        "grooming": 1,
+        "health_issues": 1,
+        "intelligence": 5,
+        "shedding_level": 3,
+        "social_needs": 5,
+        "stranger_friendly": 2,
+        "vocalisation": 3,
+        "experimental": 0,
+        "hairless": 0,
+        "natural": 0,
+        "rare": 1,
+        "rex": 0,
+        "suppressed_tail": 0,
+        "short_legs": 0,
+        "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+        "hypoallergenic": 0,
+        "reference_image_id": "DbwiefiaY",
+        "image": {
+          "id": "DbwiefiaY",
+          "width": 1200,
+          "height": 627,
+          "url": "https://cdn2.thecatapi.com/images/DbwiefiaY.png"
+        }
+      }
+    ];
+    const heroImage = {
+      "id": "DbwiefiaY",
+      "url": "https://cdn2.thecatapi.com/images/DbwiefiaY.png",
+      "breeds": [
+        {
+          "weight": {
+            "imperial": "7 - 11",
+            "metric": "3 - 5"
+          },
+          "id": "kora",
+          "name": "Korat",
+          "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+          "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+          "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+          "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+          "origin": "Thailand",
+          "country_codes": "TH",
+          "country_code": "TH",
+          "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+          "life_span": "10 - 15",
+          "indoor": 0,
+          "adaptability": 5,
+          "affection_level": 5,
+          "child_friendly": 4,
+          "dog_friendly": 5,
+          "energy_level": 3,
+          "grooming": 1,
+          "health_issues": 1,
+          "intelligence": 5,
+          "shedding_level": 3,
+          "social_needs": 5,
+          "stranger_friendly": 2,
+          "vocalisation": 3,
+          "experimental": 0,
+          "hairless": 0,
+          "natural": 0,
+          "rare": 1,
+          "rex": 0,
+          "suppressed_tail": 0,
+          "short_legs": 0,
+          "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+          "hypoallergenic": 0,
+          "reference_image_id": "DbwiefiaY"
+        }
+      ],
+      "width": 1200,
+      "height": 627
+    }
+    const otherImages = [
+      {
+        "breeds": [
+          {
+            "weight": {
+              "imperial": "7 - 11",
+              "metric": "3 - 5"
+            },
+            "id": "kora",
+            "name": "Korat",
+            "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+            "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+            "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+            "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+            "origin": "Thailand",
+            "country_codes": "TH",
+            "country_code": "TH",
+            "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+            "life_span": "10 - 15",
+            "indoor": 0,
+            "adaptability": 5,
+            "affection_level": 5,
+            "child_friendly": 4,
+            "dog_friendly": 5,
+            "energy_level": 3,
+            "grooming": 1,
+            "health_issues": 1,
+            "intelligence": 5,
+            "shedding_level": 3,
+            "social_needs": 5,
+            "stranger_friendly": 2,
+            "vocalisation": 3,
+            "experimental": 0,
+            "hairless": 0,
+            "natural": 0,
+            "rare": 1,
+            "rex": 0,
+            "suppressed_tail": 0,
+            "short_legs": 0,
+            "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+            "hypoallergenic": 0,
+            "reference_image_id": "DbwiefiaY"
+          }
+        ],
+        "id": "1TcC-kiI1",
+        "url": "https://cdn2.thecatapi.com/images/1TcC-kiI1.jpg",
+        "width": 1080,
+        "height": 1350
+      },
+      {
+        "breeds": [
+          {
+            "weight": {
+              "imperial": "7 - 11",
+              "metric": "3 - 5"
+            },
+            "id": "kora",
+            "name": "Korat",
+            "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+            "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+            "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+            "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+            "origin": "Thailand",
+            "country_codes": "TH",
+            "country_code": "TH",
+            "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+            "life_span": "10 - 15",
+            "indoor": 0,
+            "adaptability": 5,
+            "affection_level": 5,
+            "child_friendly": 4,
+            "dog_friendly": 5,
+            "energy_level": 3,
+            "grooming": 1,
+            "health_issues": 1,
+            "intelligence": 5,
+            "shedding_level": 3,
+            "social_needs": 5,
+            "stranger_friendly": 2,
+            "vocalisation": 3,
+            "experimental": 0,
+            "hairless": 0,
+            "natural": 0,
+            "rare": 1,
+            "rex": 0,
+            "suppressed_tail": 0,
+            "short_legs": 0,
+            "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+            "hypoallergenic": 0,
+            "reference_image_id": "DbwiefiaY"
+          }
+        ],
+        "id": "CWGNkdpA7",
+        "url": "https://cdn2.thecatapi.com/images/CWGNkdpA7.jpg",
+        "width": 654,
+        "height": 549
+      },
+      {
+        "breeds": [
+          {
+            "weight": {
+              "imperial": "7 - 11",
+              "metric": "3 - 5"
+            },
+            "id": "kora",
+            "name": "Korat",
+            "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+            "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+            "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+            "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+            "origin": "Thailand",
+            "country_codes": "TH",
+            "country_code": "TH",
+            "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+            "life_span": "10 - 15",
+            "indoor": 0,
+            "adaptability": 5,
+            "affection_level": 5,
+            "child_friendly": 4,
+            "dog_friendly": 5,
+            "energy_level": 3,
+            "grooming": 1,
+            "health_issues": 1,
+            "intelligence": 5,
+            "shedding_level": 3,
+            "social_needs": 5,
+            "stranger_friendly": 2,
+            "vocalisation": 3,
+            "experimental": 0,
+            "hairless": 0,
+            "natural": 0,
+            "rare": 1,
+            "rex": 0,
+            "suppressed_tail": 0,
+            "short_legs": 0,
+            "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+            "hypoallergenic": 0,
+            "reference_image_id": "DbwiefiaY"
+          }
+        ],
+        "id": "xPuWATvp9",
+        "url": "https://cdn2.thecatapi.com/images/xPuWATvp9.jpg",
+        "width": 1080,
+        "height": 1350
+      },
+      {
+        "breeds": [
+          {
+            "weight": {
+              "imperial": "7 - 11",
+              "metric": "3 - 5"
+            },
+            "id": "kora",
+            "name": "Korat",
+            "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+            "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+            "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+            "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+            "origin": "Thailand",
+            "country_codes": "TH",
+            "country_code": "TH",
+            "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+            "life_span": "10 - 15",
+            "indoor": 0,
+            "adaptability": 5,
+            "affection_level": 5,
+            "child_friendly": 4,
+            "dog_friendly": 5,
+            "energy_level": 3,
+            "grooming": 1,
+            "health_issues": 1,
+            "intelligence": 5,
+            "shedding_level": 3,
+            "social_needs": 5,
+            "stranger_friendly": 2,
+            "vocalisation": 3,
+            "experimental": 0,
+            "hairless": 0,
+            "natural": 0,
+            "rare": 1,
+            "rex": 0,
+            "suppressed_tail": 0,
+            "short_legs": 0,
+            "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+            "hypoallergenic": 0,
+            "reference_image_id": "DbwiefiaY"
+          }
+        ],
+        "id": "BbsNPAeop",
+        "url": "https://cdn2.thecatapi.com/images/BbsNPAeop.jpg",
+        "width": 645,
+        "height": 380
+      },
+      {
+        "breeds": [
+          {
+            "weight": {
+              "imperial": "7 - 11",
+              "metric": "3 - 5"
+            },
+            "id": "kora",
+            "name": "Korat",
+            "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+            "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+            "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+            "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+            "origin": "Thailand",
+            "country_codes": "TH",
+            "country_code": "TH",
+            "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+            "life_span": "10 - 15",
+            "indoor": 0,
+            "adaptability": 5,
+            "affection_level": 5,
+            "child_friendly": 4,
+            "dog_friendly": 5,
+            "energy_level": 3,
+            "grooming": 1,
+            "health_issues": 1,
+            "intelligence": 5,
+            "shedding_level": 3,
+            "social_needs": 5,
+            "stranger_friendly": 2,
+            "vocalisation": 3,
+            "experimental": 0,
+            "hairless": 0,
+            "natural": 0,
+            "rare": 1,
+            "rex": 0,
+            "suppressed_tail": 0,
+            "short_legs": 0,
+            "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+            "hypoallergenic": 0,
+            "reference_image_id": "DbwiefiaY"
+          }
+        ],
+        "id": "OfIuuOv07",
+        "url": "https://cdn2.thecatapi.com/images/OfIuuOv07.jpg",
+        "width": 1200,
+        "height": 650
+      },
+      {
+        "breeds": [
+          {
+            "weight": {
+              "imperial": "7 - 11",
+              "metric": "3 - 5"
+            },
+            "id": "kora",
+            "name": "Korat",
+            "cfa_url": "http://cfa.org/Breeds/BreedsKthruR/Korat.aspx",
+            "vetstreet_url": "http://www.vetstreet.com/cats/korat",
+            "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/korat",
+            "temperament": "Active, Loyal, highly intelligent, Expressive, Trainable",
+            "origin": "Thailand",
+            "country_codes": "TH",
+            "country_code": "TH",
+            "description": "The Korat is a natural breed, and one of the oldest stable cat breeds. They are highly intelligent and confident cats that can be fearless, although they are startled by loud sounds and sudden movements. Korats form strong bonds with their people and like to cuddle and stay nearby.",
+            "life_span": "10 - 15",
+            "indoor": 0,
+            "adaptability": 5,
+            "affection_level": 5,
+            "child_friendly": 4,
+            "dog_friendly": 5,
+            "energy_level": 3,
+            "grooming": 1,
+            "health_issues": 1,
+            "intelligence": 5,
+            "shedding_level": 3,
+            "social_needs": 5,
+            "stranger_friendly": 2,
+            "vocalisation": 3,
+            "experimental": 0,
+            "hairless": 0,
+            "natural": 0,
+            "rare": 1,
+            "rex": 0,
+            "suppressed_tail": 0,
+            "short_legs": 0,
+            "wikipedia_url": "https://en.wikipedia.org/wiki/Korat",
+            "hypoallergenic": 0,
+            "reference_image_id": "DbwiefiaY"
+          }
+        ],
+        "id": "DbwiefiaY",
+        "url": "https://cdn2.thecatapi.com/images/DbwiefiaY.png",
+        "width": 1200,
+        "height": 627
+      }
+    ];
+
+    spyOn(apiHttpClientService, 'getBreedBySearchTerm').and.returnValue(of(breedData));
+    spyOn(apiHttpClientService, 'getHeroImage').and.returnValue(of(heroImage));
+    spyOn(apiHttpClientService, 'getOtherImageUrls').and.returnValue(of(otherImages));
+
+    component.name = 'korat';
+    component.ngOnInit();
+
+    expect(component.data).toEqual({ hero: heroImage, images: otherImages });
+    expect(component.loading).toBeFalse();
+    //expect(component.filteredImages).toEqual(otherImages);
+  });
+
+  it('should handle errors correctly', () => {
+    const error = new Error('An error occurred');
+
+    spyOn(apiHttpClientService, 'getBreedBySearchTerm').and.returnValue(of([]));
+    spyOn(apiHttpClientService, 'getHeroImage').and.returnValue(of());
+    spyOn(apiHttpClientService, 'getOtherImageUrls').and.returnValue(of());
+
+    component.name = 'invalid search term';
+    component.ngOnInit();
+
+    expect(component.loading).toBeFalse();
+    expect(component.error).toEqual(error);
+  });
+
 });
